70176ca85e540041e4ee5e364f95b2b439f88217
amishkr22 <amishkumar1028@gmail.com>
2025-01-11 19:32:57 +0530

    Package updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'fraud_sense',
+    version='0.0.1',
+    author = 'Amish Nayar',
+    author_email= 'amishkumar1028@gmail.com',
+    install_requires = [],
+    packages=find_packages()
+)
